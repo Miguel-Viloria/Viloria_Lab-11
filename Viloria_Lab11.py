776f34873217e048d7b073cf68398daa4f58d016
Miguel-Viloria <2024-2-00905@lpunetwork.edu.ph>
2024-11-07 20:04:40 +0800

    Viloria_Lab11.py

--- Viloria_Lab11.py
+++ Viloria_Lab11.py
@@ -4,24 +4,24 @@ passed = []
 
 while True:
     grade = input("Enter your Grade (or type 'done' to finish): ")
+
     if grade.lower() == "done":
         break
+    if not grade.isdigit() or int(grade) <= 39 or int(grade) >= 101:
+        print("Please enter a valid grade between 40 and 100, or 'done' to finish.")
     else:
-        try:
-            grade = int(grade)
-            grades.append(grade)
-            if grade >= 75:
-                passed.append(grade)
-            print(f"Grade is: {grade}")
-        except ValueError:
-            print("Please enter a valid grade or 'done' to finish.")
+        grade = int(grade)
+        grades.append(grade)
+        if grade >= 75:
+            passed.append(grade)
+        print(f"Grade is: {grade}")
 
 if grades:
     average = sum(grades) / len(grades)
     passing_percent = (len(passed) / len(grades)) * 100
-    
     print(f"Average Grade: {average:.2f}")
     print(f"Passing students percentage: {passing_percent:.2f}%")
+    print(f"Number of students passed: {len(passed)}")
+    print("All grades entered:", grades)
 else:
-    print("No Grades Inputted")
-
+    print("No grades inputted")
